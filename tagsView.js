2a267423e9ca511b512922ca9580482f4b22c39b
Yaser Hadi <yaserhadi@hotmail.com>
2025-06-16 12:59:39 +0300

    Update tagsView.js
    fix the close function

--- tagsView.js
+++ tagsView.js
@@ -1,45 +1,46 @@
-+42
--0
-
 $(function () {
   var visited = [];
 
   $('.menu-item').on('click', function (e) {
     e.preventDefault();
-    var path = $(this).attr('href');
+    var path  = $(this).attr('href');
     var title = $(this).text();
 
     $('.menu-item').removeClass('active');
     $(this).addClass('active');
 
-    if (!visited.some(function(v){ return v.path === path; })) {
-      visited.push({ path: path, title: title });
-      $('.tags-view-wrapper').append('<span class="tags-view-item" data-path="' + path + '">' + title + ' <span class="close">×</span></span>');
+    if (!visited.some(v => v.path === path)) {
+      visited.push({ path, title });
+      $('.tags-view-wrapper').append(
+        '<span class="tags-view-item" data-path="'+path+'" data-title="'+title+'">'+
+          title+' <span class="close">×</span></span>'
+      );
     }
     setActiveTag(path);
     $('#hint').text('The selected page is ' + title);
   });
 
-  $('.tags-view-wrapper').sortable({
-    items: '.tags-view-item',
-    tolerance: 'pointer'
-  });
+  $('.tags-view-wrapper')
+    .on('click', '.close', function (e) {
+      e.stopPropagation();
+      var $tag = $(this).parent();
+      var path = $tag.data('path');
+      visited = visited.filter(v => v.path !== path);
+      $tag.remove();
+    })
+    .on('click', '.tags-view-item', function () {
+      var path  = $(this).data('path');
+      var title = $(this).data('title');
 
-  $('.tags-view-wrapper').on('click', '.close', function (e) {
-    e.stopPropagation();
-    var $tag = $(this).parent();
-    var path = $tag.data('path');
-    visited = visited.filter(function(v){ return v.path !== path; });
-    $tag.remove();
-  });
+      $('.menu-item').removeClass('active');
+      $('.menu-item[href="'+path+'"]').addClass('active');
 
-  $('.tags-view-wrapper').on('click', '.tags-view-item', function () {
-    var path = $(this).data('path');
-    $('.menu-item[href="' + path + '"]').trigger('click');
-  });
+      setActiveTag(path);
+      $('#hint').text('The selected page is ' + title);
+    });
 
   function setActiveTag(path) {
     $('.tags-view-item').removeClass('active');
-    $('.tags-view-item[data-path="' + path + '"]').addClass('active');
+    $('.tags-view-item[data-path="'+path+'"]').addClass('active');
   }
 });
