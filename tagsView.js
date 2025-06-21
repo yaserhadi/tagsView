c1045a393b70c71ce807915a813915d4630790b0
Yaser Hadi <yaserhadi@hotmail.com>
2025-06-21 04:11:13 +0300

    Add scrollable left menu and expand items to 20

--- tagsView.js
+++ tagsView.js
@@ -1,6 +1,13 @@
 $(function () {
   var visited = [];
 
+  var $contextMenu = $('<ul class="context-menu">\n' +
+    '  <li data-action="font">Font Color</li>\n' +
+    '  <li data-action="background">Background Color</li>\n' +
+    '  <li data-action="close-all">Close All</li>\n' +
+    '</ul>').appendTo('body').hide();
+  var $menuTarget = null;
+
   function refreshVisited() {
     visited = $('.tags-view-item').map(function () {
       return { path: $(this).data('path'), title: $(this).data('title') };
@@ -45,6 +52,11 @@ $(function () {
       visited = visited.filter(v => v.path !== path);
       $tag.remove();
     })
+    .on('contextmenu', '.tags-view-item', function (e) {
+      e.preventDefault();
+      $menuTarget = $(this);
+      $contextMenu.css({ top: e.pageY, left: e.pageX }).show();
+    })
     .on('click', '.tags-view-item', function () {
       var path  = $(this).data('path');
       var title = $(this).data('title');
@@ -60,4 +72,27 @@ $(function () {
     $('.tags-view-item').removeClass('active');
     $('.tags-view-item[data-path="'+path+'"]').addClass('active');
   }
+
+  $(document).on('click', function () {
+    $contextMenu.hide();
+  });
+
+  $contextMenu.on('click', 'li', function () {
+    var action = $(this).data('action');
+    if (action === 'font') {
+      var color = prompt('Font color (#rrggbb or name)');
+      if (color) {
+        ($menuTarget || $('.tags-view-item')).css('color', color);
+      }
+    } else if (action === 'background') {
+      var color = prompt('Background color (#rrggbb or name)');
+      if (color) {
+        ($menuTarget || $('.tags-view-item')).css('background', color);
+      }
+    } else if (action === 'close-all') {
+      visited = [];
+      $('.tags-view-wrapper').empty();
+    }
+    $contextMenu.hide();
+  });
 });
